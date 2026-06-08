79086eb99a876c0f9f3c30cb9691b9cffb9d4984
stellanoumi90-cmyk <stellanoumi90@gmail.com>
2026-06-09 00:55:17 +0200

    Configuration pour le déploiement Render

--- lms/settings.py
+++ lms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!8jj9py$s-l$&xn=^@0*9sw4mu&=dkhg_tt7#em9(3h%&f$8e4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['localhost','127.0.0.1', '.render.com'] 
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
