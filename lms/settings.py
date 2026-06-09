2345ee047817395c3bb02e38529d2ba9042b14c2
stellanoumi90-cmyk <stellanoumi90@gmail.com>
2026-06-09 11:15:00 +0200

    Fix: Ajout du domaine Render dans ALLOWED_HOSTS

--- lms/settings.py
+++ lms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!8jj9py$s-l$&xn=^@0*9sw4mu&=dkhg_tt7#em9(3h%&f$8e4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1', '.render.com'] 
+ALLOWED_HOSTS = ['lms-projet-bmxp.onrender.com', '127.0.0.1', 'localhost'] 
 
 
 # Application definition
